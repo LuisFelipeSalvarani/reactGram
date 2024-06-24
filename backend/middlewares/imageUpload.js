1f78d79960c2bb2535d0c2afdadfcfd7335bf8ec
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-06-24 17:14:59 -0300

    Middleware de upload de imagens

---
+++ backend/middlewares/imageUpload.js
@@ -0,0 +1,33 @@
+const multer = require("multer")
+const path = require("path")
+
+// Destination to store image
+const imageStorage = multer.diskStorage({
+    destination: function(req, file, cd) {
+        let folder = ""
+
+        if(req.baseUrl.includes("users")) {
+            folder = "users"
+        } else if(req.baseUrl.includes("photos")) {
+            folder = "photos"
+        }
+
+        cb(null, `uploads/${folder}/`)
+    },
+    filename: (req, file, cb) => {
+        cd(null, Date.now() + path.extname(file.originalname))
+    }
+})
+
+const imageUpload = multer({
+    storage: imageStorage,
+    fileFilter(req, file, cd) {
+        if(!file.originalname.match(/\.(png|jpg)$/)) {
+            // upload only png and jpg formats
+            return cb(new Error("Por favor, envie apenas png ou jpg!"))
+        }
+        cb(undefined, true)
+    }
+})
+
+module.exports = imageUpload

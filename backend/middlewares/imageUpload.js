21fe2607577017953088fd2dfe1a2e57c41e4f96
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-06-24 17:39:27 -0300

    Atualização do usuário

--- backend/middlewares/imageUpload.js
+++ backend/middlewares/imageUpload.js
@@ -3,7 +3,7 @@ const path = require("path")
 
 // Destination to store image
 const imageStorage = multer.diskStorage({
-    destination: function(req, file, cd) {
+    destination: function(req, file, cb) {
         let folder = ""
 
         if(req.baseUrl.includes("users")) {
@@ -15,13 +15,13 @@ const imageStorage = multer.diskStorage({
         cb(null, `uploads/${folder}/`)
     },
     filename: (req, file, cb) => {
-        cd(null, Date.now() + path.extname(file.originalname))
+        cb(null, Date.now() + path.extname(file.originalname))
     }
 })
 
 const imageUpload = multer({
     storage: imageStorage,
-    fileFilter(req, file, cd) {
+    fileFilter(req, file, cb) {
         if(!file.originalname.match(/\.(png|jpg)$/)) {
             // upload only png and jpg formats
             return cb(new Error("Por favor, envie apenas png ou jpg!"))

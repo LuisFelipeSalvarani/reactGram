08e0ac382bdc5f6abfa58c432abc5c484823da14
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-07-01 17:09:27 -0300

    Slice de update de usuário

--- frontend/src/slices/userSlice.jsx
+++ frontend/src/slices/userSlice.jsx
@@ -19,6 +19,20 @@ export const profile = createAsyncThunk("user/profile", async(user, thunkAPI) =>
     return data
 })
 
+// Update user details
+export const updateProfile = createAsyncThunk("user/update", async(user, thunkAPI) => {
+    const token = thunkAPI.getState.auth.user.token
+
+    const data = await userService.updateProfile(user, token)
+
+    // Check for errors
+    if(data.errors) {
+        return thunkAPI.rejectWithValue(data.errors[0])
+    }
+
+    return data
+})
+
 export const userSlice = createSlice({
     name: "user",
     initialState,
@@ -36,6 +50,19 @@ export const userSlice = createSlice({
             state.success = true
             state.error = null
             state.user = action.payload
+        }).addCase(updateProfile.pending, (state) => {
+            state.loading = true
+            state.error = null
+        }).addCase(updateProfile.fulfilled, (state, action) => {
+            state.loading = false
+            state.success = true
+            state.error = null
+            state.user = action.payload
+            state.message = "Usuário atualizado com sucesso!"
+        }).addCase(updateProfile.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.payload
+            state.user = null
         })
     }
 })

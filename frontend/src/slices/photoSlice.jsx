bb18b3751ea25bdc4b187da77368b27b70e2ed84
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-07-02 13:25:07 -0300

    Slice de inserção de fotos

--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -11,6 +11,19 @@ const initialState = {
     message: null,
 }
 
+// Publish user photo
+export const publishPhoto = createAsyncThunk("photo/publish", async(photo, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token
+
+    const data = await photoService.publishPhoto(photo, token)
+
+    if(data.errors) {
+        return thunkAPI.rejectWithValue(data.errors[0])
+    }
+
+    return data
+})
+
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -19,6 +32,23 @@ export const photoSlice = createSlice({
             state.message = null
         }
     },
+    extraReducers: (builder) => {
+        builder.addCase(publishPhoto.pending, (state) => {
+            state.loading = true
+            state.error = null
+        }).addCase(publishPhoto.fulfilled, (state, action) => {
+            state.loading = false
+            state.success = true
+            state.error = null
+            state.photo = action.payload
+            state.photos.unshift(state.photo)
+            state.message = "Foto publicada com sucesso!"
+        }).addCase(publishPhoto.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.payload
+            state.photo = {}
+        })
+    }
 })
 
 export const { resetMessage } = photoSlice.actions

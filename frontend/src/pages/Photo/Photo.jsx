88f1f0d12e8c7991953ad116296dd919708a860d
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-07-02 16:46:28 -0300

    Componente de like e like na foto

--- frontend/src/pages/Photo/Photo.jsx
+++ frontend/src/pages/Photo/Photo.jsx
@@ -6,6 +6,7 @@ import { uploads } from "../../utils/config"
 import Message from "../../components/Message"
 import { Link } from "react-router-dom"
 import PhotoItem from "../../components/PhotoItem"
+import LikeContainer from "../../components/LikeContainer"
 
 // Hooks
 import { useEffect, useState } from "react"
@@ -13,7 +14,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { useParams } from "react-router-dom"
 
 // Redux
-import { getPhoto } from "../../slices/photoSlice"
+import { getPhoto, like } from "../../slices/photoSlice"
 
 const Photo = () => {
     const { id } = useParams()
@@ -28,6 +29,10 @@ const Photo = () => {
         dispatch(getPhoto(id))
     }, [dispatch, id])
 
+    const handleLike = () => {
+        dispatch(like(photo._id))
+    }
+
     if(loading) {
         return <p>Carregando...</p>
     }
@@ -35,6 +40,7 @@ const Photo = () => {
   return (
     <div id="photo">
         <PhotoItem photo={photo} />
+        <LikeContainer photo={photo} user={user} handleLike={handleLike} />
     </div>
   )
 }

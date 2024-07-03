f4ee4eb36642978ca2ecbdff16ba175130262044
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-07-03 13:37:44 -0300

    Slice de busca

--- frontend/src/slices/photoSlice.jsx
+++ frontend/src/slices/photoSlice.jsx
@@ -107,6 +107,15 @@ export const getPhotos = createAsyncThunk("photo/getall", async(_, thunkAPI) => 
     return data
 })
 
+// Search photo by title
+export const searchPhotos = createAsyncThunk("photo/search", async(query, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token
+
+    const data = await photoService.searchPhotos(query, token)
+
+    return data
+})
+
 export const photoSlice = createSlice({
     name: "photo",
     initialState,
@@ -214,6 +223,14 @@ export const photoSlice = createSlice({
             state.loading = true
             state.error = null
         }).addCase(getPhotos.fulfilled, (state, action) => {
+            state.loading = false
+            state.success = true
+            state.error = null
+            state.photos = action.payload
+        }).addCase(searchPhotos.pending, (state) => {
+            state.loading = true
+            state.error = null
+        }).addCase(searchPhotos.fulfilled, (state, action) => {
             state.loading = false
             state.success = true
             state.error = null

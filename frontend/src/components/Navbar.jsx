4fb5b06ea1ae2ed706564746297a987bd3329d75
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-07-03 13:34:09 -0300

    Implementação do formulário de busca

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -9,6 +9,7 @@ import { useState } from "react"
 import { useAuth } from "../hooks/useAuth"
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
+import { useQuery } from "../hooks/useQuery"
 
 // Redux
 import { logout, reset } from "../slices/authSlice"
@@ -16,6 +17,8 @@ import { logout, reset } from "../slices/authSlice"
 const Navbar = () => {
     const { auth } = useAuth()
     const { user } = useSelector((state) => state.auth)
+
+    const [query, setQuery] = useState("")
 
     const navigate = useNavigate()
 
@@ -28,12 +31,20 @@ const Navbar = () => {
         navigate("/login")
     }
 
+    const handleSearch = (e) => {
+        e.preventDefault()
+
+        if(query) {
+            return navigate(`/search?q=${query}`)
+        }
+    }
+
   return (
     <nav id="nav">
         <Link to="/">ReactGram</Link>
-        <form id="search-form">
+        <form id="search-form" onSubmit={handleSearch}>
             <BsSearch />
-            <input type="text" placeholder="Pesquisar" />
+            <input type="text" placeholder="Pesquisar" onChange={(e) => setQuery(e.target.value)} />
         </form>
         <ul id="nav-links">
             {auth ? (
